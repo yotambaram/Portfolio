0bea8fa71ebe594baf031ec64580cf592eb0f297
yotamba12 <yotambaram@gmail.com>
2020-04-02 10:00:07 -0700

    stating js

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+
+//choose one html to be the mail one
+//put the jquert script ar html
+//conect this script
+portfolioBtn = $('#portfolioBtn')
+aboutMes = $('#aboutMe')
+contactBtn = $('#contactBtn')
+
+
+// portfolio, about me and contact gona be in one html page like a social media feed.
+// my id card at the left will be static
+// the small cards at the right (portfoloi page) will be static with changing content (API)
+// make a feture can bild a new project card into the html
+// when we click on one of them, to cards of what we pushed will go up and the others down on the feed
+
+
